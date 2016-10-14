90b9d8c12966582eacc9ce17849cd8b272bc2c93
Christine Baba <baba.christine@gmail.com>
2016-10-14 16:27:05 -0500

    game changer

--- game.js
+++ game.js
@@ -2,5 +2,21 @@
  * Created by Christine on 10/8/16.
  */
 //file will randomly select a word for the player.
+var Word = require('./word.js');
 
-Game.prototype.bank = ["dog", "cat", "cow", "man", "moose", "loose"];
+function Game (){
+  this.init();
+};
+
+Game.prototype.wrds = ["dog", "cat", "cow", "man", "moose", "loose"];
+
+Game.prototype.init = function (){
+    this.word = this.random();
+    this.guesses = new Set ();
+    this.done = false;
+};
+Game.prototype.random = function() {
+    return new Word(this.wrds[Math.floor(Math.random() * this.wrds.length)]);
+};
+console.log("hello");
+module.exports = Game;
